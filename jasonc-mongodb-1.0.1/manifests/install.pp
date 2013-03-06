c4f127f63f4edf9491fe6c5a616e3245f3b8fe34
Brian P. Holt <bholt@planetholt.com>
2013-03-06 09:20:20 -0600

    properly disables IPv6 to avoid mongod warnings

--- jasonc-mongodb-1.0.1/manifests/install.pp
+++ jasonc-mongodb-1.0.1/manifests/install.pp
@@ -17,7 +17,7 @@ class mongodb::install ( $quota       = 'true',
                          $logpath     = '/var/log/mongo/mongod.log',
                          $port        = '27017',
                          $version     = 'UNSET',
-                         $ipv6        = 'false',
+                         $ipv6        = false,
 ) {
   
 include mongodb::params
